a2cc762312c6647bece80b8e2967829664d9c1c6
Sami Laine <sami.v.laine@aalto.fi>
2026-09-14 14:46:30 +0300

    Bump version

--- envkernel.py
+++ envkernel.py
@@ -20,7 +20,7 @@ LOG.setLevel(logging.INFO)
 logging.lastResort.setLevel(logging.DEBUG)
 
 
-version_info = (1, 1, 0,) # 'dev0')
+version_info = (1, 2, 0, 'dev1')
 __version__ = '.'.join(str(x) for x in version_info)
 
 KNOWN_KERNELS = {
